9ffaaba4e8a33e4401b1f8c9a01bbafc9006690c
Saras <saras.me108@gmail.com>
2020-01-06 20:36:06 +0545

    Accuracy tested using various supervised learning algorithm

--- trainNB.py
+++ trainNB.py
@@ -45,7 +45,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3,random_st
 ##Using the Naive Bayes Algorithm
 # -----------------------------------
 
-print("Using Navie Bayes")
+print("USING NAVIE BAYES")
 #forming pipeline with vectorizer and model
 pipelineNB = Pipeline([('vectorizer', CountVectorizer()),
                     ('modelNB', naive_bayes.MultinomialNB())])
@@ -70,15 +70,15 @@ loaded_model = pickle.load(open(filename1, 'rb'))
 testmodel = loaded_model.predict(X_test)
 
 #evaluating the model
-accuracy = accuracy_score(y_test, testmodel)
-print("Test Accuracy using Navies Bayes: {:.1f}%".format(accuracy*100))
+accuracy_nb = accuracy_score(y_test, testmodel)
+print("Test Accuracy using Navies Bayes: {:.1f}%".format(accuracy_nb*100))
 
 
 
 ##Using the Random Forest Algorithm
 # -----------------------------------
 
-print("Using Random Forest")
+print("USING RANDOM FOREST")
 from sklearn.ensemble import RandomForestClassifier
 #forming pipeline using the random forest
 pipelineRf = Pipeline([('vectorizer', CountVectorizer()),
@@ -102,32 +102,57 @@ pickle.dump(trainmodelrf, open(filenamerf, 'wb'))
 
 loaded_rf_model = pickle.load(open(filenamerf, 'rb'))
 
-trainmodelrf = loaded_model.predict(X_test)
+trainmodelrf = loaded_rf_model.predict(X_test)
 accuracyrf = accuracy_score(y_test, trainmodelrf)
 print("Test Accuracy Using Random Forest: {:.1f}%".format(accuracyrf*100))
 
 
-##Using the K-means Algorithm
+
+##Using the K-Nearest Algorithm
 # -----------------------------------
 
-print("Using K-means Algorithm")
+print("USING K-NEAREST ALGORITHM")
+from sklearn.neighbors import NearestCentroid
 
-from sklearn.cluster import KMeans
+pipeline_near = Pipeline([('vectorizer', CountVectorizer()),
+                    ('modelRF', NearestCentroid())])
 
-pipeline_kmeans = Pipeline([('vectorizer', CountVectorizer()),
-                    ('modelRF', KMeans(n_clusters=2, random_state=0))])
+trainmodel_knear = pipeline_near.fit(X_train, y_train)
 
-scoresrf = cross_val_score(pipelineRf, X_train, y_train, cv=10)
-print(scoresrf)
-print("Accuracy: %0.2f " % (scoresrf.mean()))
+score_near= cross_val_score(trainmodel_knear, X_train, y_train, cv=10)
+print(score_near)
+print("Accuracy: %0.2f " % (score_near.mean()))
+
+k_meansfile = 'k_means.sav'
+pickle.dump(trainmodel_knear, open(k_meansfile, 'wb'))
+loaded_knear = pickle.load(open(k_meansfile, 'rb'))
+trainmodel_knear = loaded_knear.predict(X_test)
+accuracy_knear = accuracy_score(y_test, trainmodel_knear)
+print("Test Accuracy using k nerest: {:.2f}%".format(accuracy_knear*100))
 
 
-filenamerf = 'kmeansmodel.sav'
-pickle.dump(trainmodelrf, open(filenamerf, 'wb'))
 
-loaded_rf_model = pickle.load(open(filenamerf, 'rb'))
 
-trainmodelrf = loaded_model.predict(X_test)
+##Using the Logistic Algorithm
+# -----------------------------------
 
-accuracyrf = accuracy_score(y_test, trainmodelrf)
-print("Test Accuracy using K-means Algorithm: {:.1f}%".format(accuracyrf*100))
+print("USING LOGISTIC REGRESSION ALGORITHM")
+
+from sklearn.linear_model import LogisticRegression
+pipeline_logis = Pipeline([('vectorizer', CountVectorizer()),
+                    ('modelRF', LogisticRegression(random_state=0))])
+
+trainmodel_logis = pipeline_logis.fit(X_train, y_train)
+
+score_logis= cross_val_score(trainmodel_logis, X_train, y_train, cv=10)
+print(score_logis)
+print("Accuracy: %0.2f " % (score_logis.mean()))
+
+logis = 'logis.sav'
+pickle.dump(trainmodel_logis, open(logis, 'wb'))
+loaded_logis = pickle.load(open(logis, 'rb'))
+
+trainmodel_logis = loaded_logis.predict(X_test)
+
+accuracy_logis = accuracy_score(y_test, trainmodel_logis)
+print("Test Accuracy using Logistic regression : {:.2f}%".format(accuracy_logis*100))
